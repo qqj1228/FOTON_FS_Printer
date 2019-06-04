9eb898c7b069574284690600c81ee87ac71a6cab
qqj1228 <qqj1228@gmail.com>
2019-06-04 11:11:58 +0800

    added one special handle for ABS valve result

--- FOTON_FS_Printer/Report.cs
+++ FOTON_FS_Printer/Report.cs
@@ -75,12 +75,24 @@ namespace FOTON_FS_Printer {
         }
 
         /// <summary>
-        /// 特殊处理ABSResult项，如果ABS_DTC_Des项为不合格的话ABSResult项也为不合格，
-        /// 其余情况不处理，例如ABS_DTC_Des == "---"，ABS未检测到DTC结果
+        /// 特殊处理ABS结果项，含两处特殊处理
+        /// 1、处理ABS阀结果项。如果用于老线并且ABS总结果为合格，则ABS阀的分项结果不管原来的值为多少都为合格。
+        /// 2、处理ABSResult项。如果ABS_DTC_Des项为不合格的话ABSResult项也为不合格，其余情况不处理，例如ABS_DTC_Des == "---"，ABS未检测到DTC结果
         /// </summary>
         /// <param name="dicData"></param>
         void HandleABSResult(Dictionary<string, string> dicData) {
             if (Cfg.ColumnDic.ContainsKey("ABS_DTC_Des") && Cfg.ColumnDic.ContainsKey("ABSResult")) {
+                bool ContainsABSKey = dicData.ContainsKey(Cfg.ColumnDic["ABS_ValveFL"])
+                    && dicData.ContainsKey(Cfg.ColumnDic["ABS_ValveFR"])
+                    && dicData.ContainsKey(Cfg.ColumnDic["ABS_ValveRL"])
+                    && dicData.ContainsKey(Cfg.ColumnDic["ABS_ValveRR"])
+                    && dicData.ContainsKey(Cfg.ColumnDic["ABSResult"]);
+                if (ContainsABSKey && Cfg.Main.NewTestLine != 1 && NormalizeResult(dicData[Cfg.ColumnDic["ABSResult"]]) == "OK") {
+                    dicData[Cfg.ColumnDic["ABS_ValveFL"]] = dicData[Cfg.ColumnDic["ABSResult"]];
+                    dicData[Cfg.ColumnDic["ABS_ValveFR"]] = dicData[Cfg.ColumnDic["ABSResult"]];
+                    dicData[Cfg.ColumnDic["ABS_ValveRL"]] = dicData[Cfg.ColumnDic["ABSResult"]];
+                    dicData[Cfg.ColumnDic["ABS_ValveRR"]] = dicData[Cfg.ColumnDic["ABSResult"]];
+                }
                 if (dicData.ContainsKey(Cfg.ColumnDic["ABS_DTC_Des"]) && dicData.ContainsKey(Cfg.ColumnDic["ABSResult"])) {
                     if (int.TryParse(dicData[Cfg.ColumnDic["ABS_DTC_Des"]], out int result)) {
                         if (result <= 0) {
@@ -93,10 +105,24 @@ namespace FOTON_FS_Printer {
             }
         }
 
+        /// <summary>
+        /// 特殊处理报表中的两个时间项，均改为当前时间
+        /// </summary>
+        /// <param name="content">整个检测报表的内容</param>
+        /// <returns>处理时间后的检测报表内容</returns>
+        string HandleDateTime(string content) {
+            string ret = content;
+            string now = DateTime.Now.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss");
+            ret = ret.Replace("$LeaveFactoryTime$", now);
+            ret = ret.Replace("$TestTime$", now);
+            return ret;
+        }
+
         string ReplaceData(string ori, Dictionary<string, string> dicData) {
             string ret = ori;
             bool totalResult = true;
             HandleABSResult(dicData);
+            ret = HandleDateTime(ret);
             foreach (var item in Cfg.ColumnDic) {
                 if (!dicData.ContainsKey(item.Value)) {
                     // 模板内的报表项不包含在结果数据dicData中
@@ -153,6 +179,28 @@ namespace FOTON_FS_Printer {
 
         public string GetReportFile(string fileName) {
             return this.StrTempPath + fileName + ".htm";
+        }
+
+        string NormalizeResult(string value) {
+            string ret = "OK";
+            if (int.TryParse(value, out int result)) {
+                if (result > 0) {
+                    ret = "OK";
+                } else {
+                    ret = "不合格";
+                }
+            } else {
+                if (value.Contains('O') || value.Contains('o') || value.Contains('P') || value.Contains('p')) {
+                    if (value.Contains('N') || value.Contains('n')) {
+                        ret = "不合格";
+                    } else {
+                        ret = "OK";
+                    }
+                } else {
+                    ret = "不合格";
+                }
+            }
+            return ret;
         }
     }
 }
